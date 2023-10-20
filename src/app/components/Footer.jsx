71c2679500b17901089cf6e5c4c8c4da63562fc8
sara-pulido <sara01rizo@gmail.com>
2023-10-20 11:47:15 +0200

    Change container from page file

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="py-6 dark:bg-gray-800 dark:text-gray-50">
-      <div className="container px-0 mx-auto space-y-6 divide-y divide-gray-400 md:space-y-12 divide-opacity-50">
+      <div className="container px-4 mx-auto space-y-6 divide-y divide-gray-400 md:space-y-12 divide-opacity-50">
         <div className="grid grid-cols-12">
           <div className="pb-6 col-span-full md:pb-0 md:col-span-6">
             <Link rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 md:justify-start">
