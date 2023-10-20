b41d4a9347cb04fa4f3da896c74f44ec8f5e5b9d
sara-pulido <sara01rizo@gmail.com>
2023-10-20 19:34:29 +0200

    Fix media q

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,11 +21,6 @@ module.exports = {
     },
     screens: {
       'xs': {'min': '475px', 'max': '639px'},
-      'sm': {'min': '640px', 'max': '767px'},
-      'md': {'min': '768px', 'max': '1023px'},
-      'lg': {'min': '1024px', 'max': '1279px'},
-      'xl': {'min': '1280px', 'max': '1535px'},
-      '2xl': {'min': '1536px'},
     },
   },
   plugins: [],
