3a1a78c064fb4ebe8dbf4fd7e7768d727f24e78f
sara-pulido <sara01rizo@gmail.com>
2023-10-20 19:30:30 +0200

    More media q

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -28,8 +28,8 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed mx-auto border border-[#fdaef9] top-0 left-0 xs sm right-0 z-10 bg-pink-100 bg-opacity-100">
-      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto xs sm px-4 py-2">
+    <nav className="fixed mx-auto border border-[#fdaef9] top-0 left-0 right-0 z-10 bg-pink-100 bg-opacity-100">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
           className="text-2xl md:text-5xl text-slate-700 font-semibold"
